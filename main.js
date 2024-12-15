ece69aa43327364d24f3f835c3c6b74eea8f87a3
Ubik-Lembud12 <psulthanrama@gmail.com>
2024-12-15 17:04:02 +0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,57 @@
+/*===== MENU SHOW Y HIDDEN =====*/
+const navMenu = document.getElementById('nav-menu'),
+    toggleMenu = document.getElementById('nav-toggle'),
+    closeMenu = document.getElementById('nav-close')
+
+// SHOW
+toggleMenu.addEventListener('click', ()=>{
+    navMenu.classList.toggle('show')
+})
+
+// HIDDEN
+closeMenu.addEventListener('click', ()=>{
+    navMenu.classList.remove('show')
+})
+
+/*===== MOUSEMOVE HOME IMG =====*/
+document.addEventListener('mousemove', move);
+function move(e){
+    this.querySelectorAll('.move').forEach(layer =>{
+        const speed = layer.getAttribute('data-speed')
+
+        const x = (window.innerWidth - e.pageX*speed)/120
+        const y = (window.innerHeight - e.pageY*speed)/120
+
+        layer.style.transform = `translateX(${x}px) translateY(${y}px)`
+    })
+}
+
+/*===== GSAP ANIMATION =====*/
+// NAV
+gsap.from('.nav__logo, .nav__toggle', {opacity: 0, duration: 1, delay:2, y: 10})
+gsap.from('.nav__item', {opacity: 0, duration: 1, delay: 2.1, y: 30, stagger: 0.2,})
+
+// HOME
+gsap.from('.home__title', {opacity: 0, duration: 1, delay:1.6, y: 30})
+gsap.from('.home__description', {opacity: 0, duration: 1, delay:1.8, y: 30})
+gsap.from('.home__button', {opacity: 0, duration: 1, delay:2.1, y: 30})
+gsap.from('.home__img', {opacity: 0, duration: 1, delay:1.3, y: 30})
+
+// Smooth scrolling for internal links
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+      e.preventDefault();
+  
+      // Get the target element's position
+      const targetId = this.getAttribute('href').substring(1);
+      const targetElement = document.getElementById(targetId);
+  
+      // Scroll to the target element with offset
+      window.scrollTo({
+        top: targetElement.offsetTop - document.querySelector('header').offsetHeight, // Offset header height
+        behavior: 'smooth'
+      });
+    });
+  });
+
+ 
